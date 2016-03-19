051166dcf545e2b4612f1ce6fd2e2f80155b4620
LGOLIK <golikster@gmail.com>
2016-03-19 13:17:53 -0400

    more search results details rendering

--- public/js/components/single_result.js
+++ public/js/components/single_result.js
@@ -6,11 +6,28 @@ const SingleResult = React.createClass({
     this.props.onclick(this.props.index);
   },
   render() {
+    let address = '';
+    let lon = '';
+    let lat = '';
+    if (this.props.resultdata.venue) {
+      address = this.props.resultdata.venue.address_1;
+      lon = this.props.resultdata.venue.lon;
+      lat = this.props.resultdata.venue.lat;
+    }
+
     return (
       <div>
         <h3>{this.props.resultdata.name}</h3>
         <p>{this.props.resultdata.time}</p>
         <p>{this.props.searchParam.category}</p>
+        <p>{address}</p>
+        <p>{lon}</p>
+        <p>{lat}</p>
+        <p>{this.props.searchParam.city}</p>
+        <p>{this.props.searchParam.state}</p>
+        <p>{this.props.searchParam.country}</p>
+        <p>{this.props.resultdata.event_url}</p>
+        <p></p>
       </div>
 
     )
