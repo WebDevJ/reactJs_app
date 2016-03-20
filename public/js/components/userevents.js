4417965e98a60dedbfd5374b96975f846fdfd0c5
jvollherbst <jasmine.cardoza@gmail.com>
2016-03-20 15:34:32 -0400

    can render events user is attending on my events page

--- public/js/components/userevents.js
+++ public/js/components/userevents.js
@@ -13,21 +13,113 @@ const $               = require('jquery');
 
 // routes to helpers go here
 const auth     = require('../helpers/auth');
+const Nav      = require('./nav');
+const Footer   = require('./footer');
 const Events   = require('./events');
 const PastEvents = require('./pastevents');
 
 
 const UserEvents = React.createClass({
+  getInitialState() {
+    return {
+      me: '',
+      myevents: {}
+    }
+  },
+
+  componentDidMount(){
+    $.ajax({
+      url: 'users/me',
+      beforeSend: function( xhr ) {
+        xhr.setRequestHeader("Authorization", "Bearer " + auth.getToken() );
+      }
+    }).done((data) => {
+      this.setState({me: [data.user_id, data.first]})
+    })
+
+    $.ajax({
+      url: '/events/me',
+      beforeSend: function( xhr ) {
+        xhr.setRequestHeader("Authorization", "Bearer " + auth.getToken() );
+      }
+    }).done( (data) => {
+      console.log(data);
+      data.forEach(el => {
+        this.state.myevents[el.event_id] = el;
+      })
+      this.setState({myevents: this.state.myevents})
+    })
+  },
+
   render() {
-    const token = auth.getToken()
 
     return (
       <div>
+      <header>
+        <Nav me={this.state.me} />
+      </header>
+      <div>
         <h1>My Events</h1>
-        <div><PastEvents events={this.props.events}/></div>
+        </div>
+
+        <div><MyEvents myevents={this.state.myevents} /></div>
+        <div><Footer /></div>
       </div>
     )
   }
 })
+//
+
+const MyEvents = React.createClass({
+
+  showMyEvents(key) {
+    return (
+      <MySingleEvent key={key} index={key} details={this.props.myevents[key]}/>
+    )
+  },
+
+  render() {
+    return (
+      <div>
+        <ul className="event-list">
+          <li>{Object.keys(this.props.myevents)
+          .map(this.showMyEvents)}</li>
+          </ul>
+      </div>
+    )
+  }
+})
+
+const MySingleEvent = React.createClass({
+  //
+  // handleDeleteClick(event) {
+  //   event.preventDefault();
+  //   this.props.deleteEvent(this.props.index)
+  // },
+//add a hidden form field to grab event id; using the form to render hidden info, see the single search form
+  render() {
+    return (
+      <div className="events">
+        <h3>{this.props.details.event_name}</h3>
+        <p>{this.props.details.event_time}</p>
+        <p>{this.props.details.address}</p>
+        <p>{this.props.details.city}</p>
+        <p><a href="{this.props.details.added_by}">{this.props.details.event_url}</a></p>
+        <p>{this.props.details.attendees}</p>
+        <p><button onClick={this.handleClick}>Add Event</button> </p>
+        <p><DeleteButton /></p>
+      </div>
+    )
+  }
+})
+
+const DeleteButton = React.createClass({
+  render() {
+    return (
+      <button onClick={this.handleDeleteClick}>Remove Event</button>
+    )
+  }
+})
+
 
 module.exports = UserEvents;
