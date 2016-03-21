e86d50c9dc68aec5b59982f67923e9038c60ee4d
jvollherbst <jasmine.cardoza@gmail.com>
2016-03-21 16:24:27 -0400

    updated css, and db

--- public/js/components/homepage.js
+++ public/js/components/homepage.js
@@ -29,26 +29,26 @@ const HomePage = React.createClass({
       <div className="hp-background">
         <div className="content">
           <div className="hp-content">
-          <h1 id="register">Heading About the App</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et.</p>
+          <h1 id="register">Enjoy The Best that NYC has to offer!</h1>
+            <p>With just one app you and your friends can join a community of like minded individuals that LOVE NYC events.</p>
             <Register />
             </div>
           </div>
         </div>
 
           <div>
-            <div className="hp-col red"><h2 className="red-text">Some text describing our awesome app. This rocks. You should use this cause it is literally the best thing. EVER.</h2></div>
+            <div className="hp-col red"><h2 className="red-text">We curate your experience by tapping into the world of MeetUp.com. In this way we are able to set up a community of events that are fun, active & engaging.</h2></div>
             <div className="hp-col"><img src="../images/screenprinting.png" /></div>
           </div>
 
           <div>
             <div className="hp-col"><img src="../images/lego.png" /></div>
-            <div className="hp-col green"><h2 className="green-text">Some text describing our awesome app. This rocks. You should use this cause it is literally the best thing. EVER.</h2></div>
+            <div className="hp-col green"><h2 className="green-text">The best part is you can see if your friends are going and if any one in the Community has already gone. No need to research & plan for weeks ahead for great actives.</h2></div>
           </div>
 
           <div className="content">
             <div className="hp-content">
-              <h1 className="blue-text">Another Heading</h1>
+              <h1 className="blue-text">Share a Great MeetUp with the Community</h1>
               <button><a href="#register">Sign Up Today</a></button>
             </div>
           </div>
